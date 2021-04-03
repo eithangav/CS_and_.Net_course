3504970c580ca0bc5ade4a0a6cf97618991d1892
Eithan Gavrieli <eitangav@gmail.com>
2021-04-03 19:58:09 +0300

    Ex01 part 2 is done

--- B21 Ex01 Eithan 204311757 Maor 204709950/B21_Ex01_2/Program.cs
+++ B21 Ex01 Eithan 204311757 Maor 204709950/B21_Ex01_2/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace B21_Ex01_2
+namespace Ex01_2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            // prints the sandClock of numberOfStars = 5
+            printSandClock(5, 5);
+            Console.WriteLine("Press 'Enter' to exit");
+            Console.ReadLine();
         }
+
+        /// <summary>
+        /// prints the desired sandClock of *
+        /// </summary>
+        /// <param name="i_NumOfStars">helper parm for the recursive call</param>
+        /// <param name="i_NumOfLines">the desired number of liness of stars</param>
+        public static void printSandClock(int i_NumOfStars, int i_NumOfLines)
+        {
+            // base condition
+            if(i_NumOfStars <= 1)
+            {
+                printStarsLine(1, i_NumOfLines);
+                return;
+            }
+
+            // prints the current line of stars
+            printStarsLine(i_NumOfStars, i_NumOfLines);
+
+            // the recursive call
+            printSandClock(i_NumOfStars - 2, i_NumOfLines);
+
+            // prints the current line of stars
+            printStarsLine(i_NumOfStars, i_NumOfLines);
+        }
+
+        /// <summary>
+        /// prints the desired number of stars and appends spaces in the beginning and in the end of the line
+        /// </summary>
+        /// <param name="i_NumOfStars"></param>
+        /// <param name="i_NumOfChars"></param>
+        public static void printStarsLine(int i_NumOfStars, int i_NumOfChars)
+        {
+            // the number of spaces at the beginning and at the end of the line
+            int numOfSpaces = (i_NumOfChars - i_NumOfStars) / 2;
+
+            // used to create the line of spaces and stars
+            StringBuilder lineOutput = new StringBuilder();
+
+            // appends spaces at the beginning of the line
+            for (int i = 0; i < numOfSpaces; i++)
+            {
+                lineOutput.Append(" ");
+            }
+
+            // appends the current amount of stars 
+            for (int i = 0; i < i_NumOfStars; i++)
+            {
+                lineOutput.Append("*");
+            }
+
+            // appends spaces at the end of the line
+            for (int i = 0; i < numOfSpaces; i++)
+            {
+                lineOutput.Append(" ");
+            }
+
+            // prints the total line of spaces and stars
+            Console.WriteLine(lineOutput.ToString());
+        }
+
     }
 }
