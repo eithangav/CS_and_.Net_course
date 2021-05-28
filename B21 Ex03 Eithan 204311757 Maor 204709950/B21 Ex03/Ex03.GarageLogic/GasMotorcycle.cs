36b30b71c7a253212da3f3d071b7021ef4f55305
Eithan Gavrieli <eitangav@gmail.com>
2021-05-28 18:00:43 +0300

    Logic Objects classes imlemented

--- B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.GarageLogic/GasMotorcycle.cs
+++ B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.GarageLogic/GasMotorcycle.cs
@@ -6,5 +6,52 @@ namespace Ex03.GarageLogic
 {
     public class GasMotorcycle: Motorcycle, IGasVehicle
     {
+        private GasType m_GasType;
+        private float m_FuelLeft;
+        private float m_MaxFuel;
+
+        public GasMotorcycle(string i_Model, string i_PlateID, LicenseType i_LicenseType, int i_EngineCapacity, GasType i_GasType, float i_FuelLeft, float i_MaxFuel) :
+            base(i_Model, i_PlateID, (i_FuelLeft / i_MaxFuel) * 100, i_LicenseType, i_EngineCapacity)
+        {
+            m_GasType = i_GasType;
+            m_FuelLeft = i_FuelLeft;
+            m_MaxFuel = i_MaxFuel;
+        }
+
+        public void Refuel(float i_Liters, GasType i_GasType)
+        {
+            if (i_GasType == m_GasType && i_Liters <= m_MaxFuel - m_FuelLeft)
+            {
+                m_FuelLeft += i_Liters;
+            }
+            else
+            {
+                // TO DO: throw exceptions (wrong gas type / too much fuel)
+            }
+        }
+
+        public GasType GasType
+        {
+            get
+            {
+                return m_GasType;
+            }
+        }
+
+        public float FuelLeft
+        {
+            get
+            {
+                return m_FuelLeft;
+            }
+        }
+
+        public float MaxFuel
+        {
+            get
+            {
+                return m_MaxFuel;
+            }
+        }
     }
 }
--- B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.GarageLogic/Motorcycle.cs
+++ B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.GarageLogic/Motorcycle.cs
@@ -6,6 +6,31 @@ namespace Ex03.GarageLogic
 {
     public class Motorcycle: Vehicle
     {
+        private LicenseType m_LiscenceType;
+        private int m_EngineCapacity;
+
+        public Motorcycle(string i_Model, string i_PlateID, float i_EnergyLeft, LicenseType i_LicenseType, int i_EngineCapacity):
+            base(i_Model, i_PlateID, i_EnergyLeft)
+        {
+            m_LiscenceType = i_LicenseType;
+            m_EngineCapacity = i_EngineCapacity;
+        }
+
+        public LicenseType LicenseType
+        {
+            get
+            {
+                return m_LiscenceType;
+            }
+        }
+
+        public int EngineCapacity
+        {
+            get
+            {
+                return m_EngineCapacity;
+            }
+        }
     }
 
     public enum LicenseType
