a2e8090e844fec033d63d2762c23e8838004a3d5
Eithan Gavrieli <eitangav@gmail.com>
2021-06-02 21:14:49 +0300

    GarageUI insert vehicle work

--- B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.ConsoleUI/GarageUI.cs
+++ B21 Ex03 Eithan 204311757 Maor 204709950/B21 Ex03/Ex03.ConsoleUI/GarageUI.cs
@@ -34,29 +34,14 @@ namespace Ex03.ConsoleUI
 
             while (true)
             {
-                byte operationToExecute = MainMenu();
-
-                if(operationToExecute == 1)
-                {
-                    insertNewVehicle();
-                }
-                else if(operationToExecute == 2)
-                {
-
-                }
-                // ... complete all other options ...
-                else if(operationToExecute == 8)
-                {
-                    killConsoleUI();
-                }
-
+                MainMenu();                
                 Console.Clear();
             }
         }
 
-        private byte MainMenu()
+        private void MainMenu()
         {
-            Console.WriteLine(string.Format("Please choose an action: (enter a number between 1-8)" +
+            Console.WriteLine("Please choose an action: (enter a number between 1-8)" +
                 "\n1. Insert new vehicle" +
                 "\n2. List the garage's plate IDs" +
                 "\n3. Change vehicle's status" +
@@ -64,12 +49,40 @@ namespace Ex03.ConsoleUI
                 "\n5. Refuel a gas vehicle" +
                 "\n6. Charge an electric vehicle" +
                 "\n7. Show vehicle's details" +
-                "\n8. Exit"));
+                "\n8. Exit");
 
-            return  readAndValidateMenuNumInput(8);
+            byte sectionToExecute = readAndValidateMenuChoiceInput(8);
+
+            switch (sectionToExecute)
+            {
+                case 1:
+                    insertNewVehicle();
+                    break;
+                case 2:
+                    showGaragePlatesId();
+                    break;
+                case 3:
+                    changeVehicleState();
+                    break;
+                case 4:
+                    inflateWheels();
+                    break;
+                case 5:
+                    refuelGasVehicle();
+                    break;
+                case 6:
+                    chargeElectricVehicle();
+                    break;
+                case 7:
+                    inflateWheels();
+                    break;
+                case 8:
+                    killConsoleUI();
+                    break;
+            }
         }
 
-        private byte readAndValidateMenuNumInput(byte i_MaxValue)
+        private byte readAndValidateMenuChoiceInput(byte i_MaxValue)
         {
             string userChoice;
             byte userChoiceNum = 0;
@@ -99,29 +112,463 @@ namespace Ex03.ConsoleUI
             return userChoiceNum;
         }
 
-        private void killConsoleUI()
+        private string readAndValidateStringInput()
         {
-            // TODO: implement ending the program
+            string invalidInputMsg = "Invalid input. Please enter a non-empty string";
+            string inputStr = "";
+
+            while(inputStr == "")
+            {
+                inputStr = Console.ReadLine();
+
+                if(inputStr == "")
+                {
+                    Console.WriteLine(invalidInputMsg);
+                }
+            }
+
+            return inputStr;
+        }
+
+        private float readAndValidateNonNegativeNumInput(bool i_RequiredPositive = false, bool i_RequiredInteger = false)
+        {
+            string inputStr;
+            float inputNum = -1;
+
+            string numPositivity = "non-negative";
+            string numType = "number";
+
+            if (i_RequiredPositive)
+            {
+                numPositivity = "positive";
+            }
+
+            if (i_RequiredInteger)
+            {
+                numType = "integer";
+            }
+
+            string invalidInputMsg = string.Format("Invalid input. Please enter a {0} {1}", numPositivity, numType);
+
+            while (inputNum == -1)
+            {
+                inputStr = Console.ReadLine();
+
+                try
+                {
+                    inputNum = float.Parse(inputStr);
+
+                    if (inputNum < 0 || (i_RequiredPositive && inputNum == 0) || (i_RequiredPositive && inputNum - (float)((int)inputNum) != 0))
+                    {
+                        inputNum = -1;
+                        Console.WriteLine(invalidInputMsg);
+                    }
+                }
+                catch
+                {
+                    inputNum = -1;
+                    Console.WriteLine(invalidInputMsg);
+                }
+            }
+
+            return inputNum;
         }
 
         private void insertNewVehicle()
         {
+            try
+            {
+                Console.WriteLine("Insert the vehicle's Plate ID:");
+                string plateId = Console.ReadLine();
 
+                if (m_Garage.VehicleExists(plateId))
+                {
+                    m_Garage.ChangeVehicleState(plateId, VehicleStatus.InProgress);
+                    Console.WriteLine("This plate ID already exists. It's vehicle's status was updated to 'In proress'");
+                }
+                else
+                {
+                    vehicleInsertionMenu();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An error occured. Please try again....");
+            }
+        }
+
+        private void vehicleInsertionMenu()
+        {
+            Console.WriteLine("Which vehicle would you like to insert? (enter a number between 1-5)" +
+                       "\n1. Gas car" +
+                       "\n2. Electric car" +
+                       "\n3. Gas motorcycle" +
+                       "\n4. Electric motorcycle" +
+                       "\n5. Truck");
+
+            byte chosenSection = readAndValidateMenuChoiceInput(5);
+
+            switch (chosenSection)
+            {
+                case 1:
+                    insertVehicleByType(VehicleType.GasCar);
+                    break;
+                case 2:
+                    insertVehicleByType(VehicleType.ElectricCar);
+                    break;
+                case 3:
+                    insertVehicleByType(VehicleType.GasMotorcycle);
+                    break;
+                case 4:
+                    insertVehicleByType(VehicleType.ElectricMotorcycle);
+                    break;
+                case 5:
+                    insertVehicleByType(VehicleType.Truck);
+                    break;
+            }
+        }
+
+        private void insertVehicleByType(VehicleType i_VehicleType)
+        {
+            m_Garage.InsertGasCar();
+            m_Garage.InsertElectricCar();
+            m_Garage.InsertGasMotorcycle();
+            m_Garage.InsertElectricMotorcycle();
+            m_Garage.InsertTruck();
+
+            string vehicleType = stringifyVehicleType(i_VehicleType);
+
+            // Read shared properties:
+
+            Console.WriteLine("Enter customer's name:");
+            string customerName = readAndValidateStringInput();
+
+            Console.WriteLine("Enter customer's phone:");
+            string customerPhone = readAndValidateStringInput();
+
+            Console.WriteLine(string.Format("Enter the {0}'s model:", vehicleType));
+            string model = readAndValidateStringInput();
+
+            Console.WriteLine(string.Format("Enter the {0}'s plate ID:", vehicleType));
+            string plateId = readAndValidateStringInput();
+
+            string[] wheelsManucafturers;
+            float[] wheelsCurrentAirPressures;
+            readWheelsProperties(i_VehicleType, out wheelsManucafturers, out wheelsCurrentAirPressures);
+
+            GasType gasType;
+            float fuelLeft;
+            float maxFuel;
+            float batteryLeft;
+            float maxBatteryTime;
+
+            bool electric = isElectric(i_VehicleType);
+
+            if (vehicleType == "car")
+            {
+                Color color;
+                NumOfDoors numOfDoors;
+                readCarUniqueProperties(out color, out numOfDoors);
+
+                if (electric)
+                {
+                    readElectricVehicleUniqueProperties(i_VehicleType, out batteryLeft, out maxBatteryTime);
+                    m_Garage.InsertElectricCar(model, plateId, color, numOfDoors, batteryLeft, maxBatteryTime, wheelsManucafturers, wheelsCurrentAirPressures, customerName, customerPhone);
+
+                }
+                else
+                {
+                    readGasVehicleUniqueProperties(i_VehicleType, out gasType, out fuelLeft, out maxFuel);
+                    m_Garage.InsertGasCar(model, plateId, color, numOfDoors, gasType, fuelLeft, maxFuel, wheelsManucafturers, wheelsCurrentAirPressures, customerName, customerPhone);
+                }
+            }
+            else if(vehicleType == "motorcycle")
+            {
+                LicenseType licenseType;
+                int engineCapacity;
+                readMotorcycleUniqueProperties(out licenseType, out engineCapacity);
+
+                if (electric)
+                {
+
+                }
+            }
+        }
+
+        private void readWheelsProperties(VehicleType i_VehicleType, out string[] i_Manufacturers, out float[] i_CurrentAirPressures)
+        {
+            byte numOfWheels = getNumOfWheels(i_VehicleType);
+
+            i_Manufacturers = new string[numOfWheels];
+            i_CurrentAirPressures = new float[numOfWheels];
+
+            for(int i=0; i<numOfWheels; i++)
+            {
+                Console.WriteLine(string.Format("Enter wheel {0} manufacturer:"), i+1);
+                i_Manufacturers[i] = readAndValidateStringInput();
+
+                Console.WriteLine(string.Format("Enter wheel {0} current air pressure:"), i + 1);
+                i_CurrentAirPressures[i] = readAndValidateNonNegativeNumInput();
+            }
+        }
+
+        private void readCarUniqueProperties(out Color i_Color, out NumOfDoors i_NumOfDoors)
+        {
+            Console.WriteLine("What is the car's color? (enter a number between 1-4)" +
+                       "\n1. Red" +
+                       "\n2. Silver" +
+                       "\n3. White" +
+                       "\n4. Black");
+            
+            byte chosenNumber = readAndValidateMenuChoiceInput(4);
+            Color color = Color.Red;
+
+            switch (chosenNumber)
+            {
+                case 1:
+                    color = Color.Red;
+                    break;
+                case 2:
+                    color = Color.Silver;
+                    break;
+                case 3:
+                    color = Color.White;
+                    break;
+                case 4:
+                    color = Color.Black;
+                    break;
+            }
+
+            i_Color = color;
+
+            Console.WriteLine("How many doors? (enter a number between 1-4)" +
+                       "\n1. Two" +
+                       "\n2. Three" +
+                       "\n3. Four" +
+                       "\n4. Five");
+
+            chosenNumber = readAndValidateMenuChoiceInput(4);
+            NumOfDoors numOfDoors = NumOfDoors.Two;
+
+            switch (chosenNumber)
+            {
+                case 1:
+                    i_NumOfDoors = NumOfDoors.Two;
+                    break;
+                case 2:
+                    i_NumOfDoors = NumOfDoors.Three;
+                    break;
+                case 3:
+                    i_NumOfDoors = NumOfDoors.Four;
+                    break;
+                case 4:
+                    i_NumOfDoors = NumOfDoors.Five;
+                    break;
+            }
+
+            i_NumOfDoors = numOfDoors;
+        }
+
+        private void readMotorcycleUniqueProperties(out LicenseType i_LicenseType, out int i_EngineCapacity)
+        {
+            Console.WriteLine("What is the motorcycle's license type? (enter a number between 1-4)" +
+                       "\n1. BB" +
+                       "\n2. AA" +
+                       "\n3. B1" +
+                       "\n4. A");
+
+            byte chosenNumber = readAndValidateMenuChoiceInput(4);
+            LicenseType licenseType = LicenseType.BB;
+
+            switch (chosenNumber)
+            {
+                case 1:
+                    licenseType = LicenseType.BB;
+                    break;
+                case 2:
+                    licenseType = LicenseType.AA;
+                    break;
+                case 3:
+                    licenseType = LicenseType.B1;
+                    break;
+                case 4:
+                    licenseType = LicenseType.A;
+                    break;
+            }
+
+            i_LicenseType = licenseType;
+
+            Console.WriteLine("What is the motorcycle's engine capacity (enter a positive integer)");
+
+            i_EngineCapacity = (int)readAndValidateNonNegativeNumInput(true, true);
+        }
+
+        private void readTruckUniqueProperties(out bool i_ContainsCimicals, out float i_MaxCargoWeight)
+        {
+            Console.WriteLine("Does the truck contain cimicals? (enter a number between 1-2)" +
+                       "\n1. Yes" +
+                       "\n2. No");
+
+            byte chosenNumber = readAndValidateMenuChoiceInput(2);
+
+            i_ContainsCimicals = chosenNumber == 1;
+
+            Console.WriteLine("What is the truck's max cargo weight (enter a non-negative number)");
+
+            i_MaxCargoWeight = readAndValidateNonNegativeNumInput();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        private void readGasVehicleUniqueProperties(VehicleType i_VehicleType, out GasType i_GasType, out float i_FuelLeft, out float i_MaxFuel)
+        {
+            if(i_VehicleType == VehicleType.ElectricCar || i_VehicleType == VehicleType.ElectricMotorcycle)
+            {
+                throw new ArgumentException();
+            }
+
+            string vehicleType = stringifyVehicleType(i_VehicleType);
+
+            Console.WriteLine(string.Format("What is the {0}'s gas type? (enter a number between 1-4)" +
+                       "\n1. Octan98" +
+                       "\n2. Octan96" +
+                       "\n3. Octan95" +
+                       "\n4. Soler", vehicleType));
+
+            byte chosenNumber = readAndValidateMenuChoiceInput(4);
+            GasType gasType = GasType.Octan98;
+
+            switch (chosenNumber)
+            {
+                case 1:
+                    gasType = GasType.Octan98;
+                    break;
+                case 2:
+                    gasType = GasType.Octan96;
+                    break;
+                case 3:
+                    gasType = GasType.Octan95;
+                    break;
+                case 4:
+                    gasType = GasType.Soler;
+                    break;
+            }
+
+            i_GasType = gasType;
+
+            Console.WriteLine(string.Format("How much fuel left in the {0}'s tank? (enter a non-negative number)", vehicleType));
+
+            i_FuelLeft = readAndValidateNonNegativeNumInput();
+
+            Console.WriteLine(string.Format("What is the maximum possible fuel of the {0}'s tank? (enter a positive number)", vehicleType));
+
+            i_MaxFuel = readAndValidateNonNegativeNumInput(true, false);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        private void readElectricVehicleUniqueProperties(VehicleType i_VehicleType, out float i_BatteryLeft, out float i_MaxBatteryTime)
+        {
+            if(i_VehicleType == VehicleType.Truck || i_VehicleType == VehicleType.GasCar || i_VehicleType == VehicleType.GasMotorcycle)
+            {
+                throw new ArgumentException();
+            }
+
+            string vehicleType = stringifyVehicleType(i_VehicleType);
+
+            Console.WriteLine(string.Format("How much time left in the {0}'s battery? (enter a non-negative number)", vehicleType));
+
+            i_BatteryLeft = readAndValidateNonNegativeNumInput();
+
+            Console.WriteLine(string.Format("What is the maximum time of the {0}'s battery? (enter a non-negative number)", vehicleType));
+
+            i_MaxBatteryTime = readAndValidateNonNegativeNumInput();
+        }
+
+        private string stringifyVehicleType(VehicleType i_VehicleType)
+        {
+            string vehicleType = "car";
+
+            if (i_VehicleType == VehicleType.ElectricMotorcycle || i_VehicleType == VehicleType.GasMotorcycle)
+            {
+                vehicleType = "motorcycle";
+            }
+            else if (i_VehicleType == VehicleType.Truck)
+            {
+                vehicleType = "truck";
+            }
+
+            return vehicleType;
+        }
+
+        private byte getNumOfWheels(VehicleType i_VehicleType)
+        {
+            byte numOfWheels = 0;
+
+            switch (i_VehicleType)
+            {
+                case VehicleType.ElectricCar:
+                    numOfWheels = 4;
+                    break;
+                case VehicleType.GasCar:
+                    numOfWheels = 4;
+                    break;
+                case VehicleType.ElectricMotorcycle:
+                    numOfWheels = 2;
+                    break;
+                case VehicleType.GasMotorcycle:
+                    numOfWheels = 2;
+                    break;
+                case VehicleType.Truck:
+                    numOfWheels = 16;
+                    break;
+            }
+
+            return numOfWheels;
+        }
+
+        private bool isElectric(VehicleType i_VehicleType)
+        {
+            return i_VehicleType == VehicleType.ElectricCar || i_VehicleType == VehicleType.ElectricMotorcycle;
         }
 
         private void showGaragePlatesId()
         {
-
+            // TODO: implement
         }
 
         private void changeVehicleState()
         {
-
+            // TODO: implement
         }
 
         private void inflateWheels()
         {
+            // TODO: implement
+        }
 
+        private void refuelGasVehicle()
+        {
+            // TODO: implement
+        }
+
+        private void chargeElectricVehicle()
+        {
+            // TODO: implement
+        }
+
+        private void showVehicleDetails()
+        {
+            // TODO: implement
+        }
+
+        private void killConsoleUI()
+        {
+            // TODO: implement ending the program
         }
 
         //Prints all the vehicles's plate id in the garage by filtered by Vehicle status.
