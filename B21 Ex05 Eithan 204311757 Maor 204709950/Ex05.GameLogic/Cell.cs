effcc36e271fd8d77e20ea4edb693b610112fbbf
Eithan Gavrieli <eitangav@gmail.com>
2021-06-18 15:09:35 +0300

    Ex05 init

---
+++ B21 Ex05 Eithan 204311757 Maor 204709950/Ex05.GameLogic/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex05.GameLogic
+{
+    public struct Cell
+    {
+        public byte m_Row;
+        public byte m_Column;
+
+        public Cell(byte i_Row, byte i_Column)
+        {
+            this.m_Row = i_Row;
+            this.m_Column = i_Column;
+
+        }
+    }
+}
