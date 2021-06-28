5bf69458daaa15bee5421f27de7e53a7a6ab6b8c
Eithan Gavrieli <eitangav@gmail.com>
2021-06-28 15:29:24 +0300

    new Ex05 solution added

---
+++ CS_and_.Net_Ex05_With_References/B21 Ex05 Eithan 204311757 Maor 204709950/Ex05.GameUI/Tournament.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex05.GameLogic
+{
+    public class Tournament
+    {
+        private byte m_Player1Score;
+        private byte m_Player2Score;
+
+        private readonly bool m_IsMultiplayer;
+        private readonly byte m_BoardSize;
+
+        private Game m_Game;
+
+        /* Constructor.
+         * Init primitives, get user's size and mode inputs
+         * and run the first game. */
+        public Tournament()
+        {
+            m_Player1Score = 0;
+            m_Player2Score = 0;
+            
+            m_BoardSize = 4; //TODO: replace with the value of the user's size input (instead of: GameUI.InsertBoardSizeMsg)
+            m_IsMultiplayer = false; //TODO: replace with the value of the user's isMultiplayer input (instead of: GameUI.IsMultiplayerMsg)
+
+            gameRun();
+        }
+
+        /* Game runner - Init Game and GameUI and iteratively runs new round
+         * until a game is ended. */
+        private void gameRun()
+        {
+            m_Game = new Game(m_BoardSize, m_IsMultiplayer);
+            // TODO: Instantiate Forms
+
+            eGameResult roundResult = eGameResult.Pending;
+            Cell usersChoice = new Cell(255,255);
+
+            while(roundResult == 0)
+            {
+                usersChoice = m_GameUI.InsertNextPlayerMoveMsg(); // TODO: modify
+                roundResult = m_Game.PlayerMove(usersChoice);
+                m_GameUI.PrintBoard(m_Game.Board); // TODO: modify
+            }
+
+            gameEnd(roundResult);
+        }
+
+        /* End a game - Prints the result, asks  */
+        private void gameEnd(eGameResult i_GameResult)
+        {
+            switch (i_GameResult) // TODO: modify all UI methods
+            {
+                case eGameResult.Quit:
+                    m_GameUI.QuitMsg();
+                    break;
+                case eGameResult.Tie:
+                    m_GameUI.TieGameMsg();
+                    break;
+                case eGameResult.PlayerTwoLose:
+                    m_GameUI.PlayerWinMsg(1);
+                    break;
+                case eGameResult.PlayerOneLose:
+                    m_GameUI.PlayerWinMsg(2);
+                    break;
+            }
+
+            m_GameUI.PointStatusMsg(m_Player1Score, m_Player2Score); // TODO: modify
+
+            if (m_GameUI.PlayAgainMsg()) // TODO: modify
+            {
+                gameRun();
+            }
+        }
+    }
+}
