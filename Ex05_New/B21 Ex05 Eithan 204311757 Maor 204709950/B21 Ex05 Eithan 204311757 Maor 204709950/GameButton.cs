9adecde18f68ab51b8a18c541648258f049f0da5
Eithan Gavrieli <eitangav@gmail.com>
2021-06-30 15:29:59 +0300

    buttons work

---
+++ Ex05_New/B21 Ex05 Eithan 204311757 Maor 204709950/B21 Ex05 Eithan 204311757 Maor 204709950/GameButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ex05.GameLogic;
+
+namespace B21_Ex05_Eithan_204311757_Maor_204709950
+{
+    internal class GameButton : Button
+    {
+        private readonly Cell r_Position;
+        private const int k_Height = 40;
+        private const int k_Width = 50;
+
+        public GameButton(Cell i_ButtonPosition)
+        {
+            r_Position = i_ButtonPosition;
+            this.Enabled = true;
+            this.Height = k_Height;
+            this.Width = k_Width;
+        }
+
+        public Cell Position
+        {
+            get
+            {
+                return r_Position;
+            }
+        }
+    }
+}
