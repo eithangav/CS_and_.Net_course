426460cee6406adbf69c87d8dcab50b097b1d6d5
Eithan Gavrieli <eitangav@gmail.com>
2021-06-30 20:13:06 +0300

    done. need to fix alerts and to document

--- Ex05_New/B21 Ex05 Eithan 204311757 Maor 204709950/B21 Ex05 Eithan 204311757 Maor 204709950/GameBoardForm.Designer.cs
+++ Ex05_New/B21 Ex05 Eithan 204311757 Maor 204709950/B21 Ex05 Eithan 204311757 Maor 204709950/GameBoardForm.Designer.cs
@@ -40,7 +40,7 @@ namespace B21_Ex05_Eithan_204311757_Maor_204709950
             this.LabelPlayer1Name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.LabelPlayer1Name.AutoSize = true;
             this.LabelPlayer1Name.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelPlayer1Name.Location = new System.Drawing.Point(156, 511);
+            this.LabelPlayer1Name.Location = new System.Drawing.Point(35, 454);
             this.LabelPlayer1Name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelPlayer1Name.Name = "LabelPlayer1Name";
             this.LabelPlayer1Name.Size = new System.Drawing.Size(70, 17);
@@ -52,7 +52,7 @@ namespace B21_Ex05_Eithan_204311757_Maor_204709950
             this.LabelPlayer2Name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.LabelPlayer2Name.AutoSize = true;
             this.LabelPlayer2Name.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelPlayer2Name.Location = new System.Drawing.Point(324, 511);
+            this.LabelPlayer2Name.Location = new System.Drawing.Point(166, 465);
             this.LabelPlayer2Name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelPlayer2Name.Name = "LabelPlayer2Name";
             this.LabelPlayer2Name.Size = new System.Drawing.Size(65, 17);
@@ -64,7 +64,7 @@ namespace B21_Ex05_Eithan_204311757_Maor_204709950
             this.LabelPlayer1Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.LabelPlayer1Score.AutoSize = true;
             this.LabelPlayer1Score.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelPlayer1Score.Location = new System.Drawing.Point(230, 511);
+            this.LabelPlayer1Score.Location = new System.Drawing.Point(360, 332);
             this.LabelPlayer1Score.Name = "LabelPlayer1Score";
             this.LabelPlayer1Score.Size = new System.Drawing.Size(18, 17);
             this.LabelPlayer1Score.TabIndex = 901;
@@ -75,7 +75,7 @@ namespace B21_Ex05_Eithan_204311757_Maor_204709950
             this.LabelPlayer2Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.LabelPlayer2Score.AutoSize = true;
             this.LabelPlayer2Score.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelPlayer2Score.Location = new System.Drawing.Point(398, 511);
+            this.LabelPlayer2Score.Location = new System.Drawing.Point(293, 440);
             this.LabelPlayer2Score.Name = "LabelPlayer2Score";
             this.LabelPlayer2Score.Size = new System.Drawing.Size(17, 17);
             this.LabelPlayer2Score.TabIndex = 902;
