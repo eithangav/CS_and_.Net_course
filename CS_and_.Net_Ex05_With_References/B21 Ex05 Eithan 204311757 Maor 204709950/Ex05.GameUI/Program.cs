cdd6aedd19f8c7c560a668e79c362625d5a9d2b4
Eithan Gavrieli <eitangav@gmail.com>
2021-06-30 11:46:22 +0300

    GameBoard testing mode

---
+++ CS_and_.Net_Ex05_With_References/B21 Ex05 Eithan 204311757 Maor 204709950/Ex05.GameUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05.GameUI
+{
+    class Program
+    {
+        static void main()
+        {
+            // TODO: run a Tournament
+        }
+    }
+}
